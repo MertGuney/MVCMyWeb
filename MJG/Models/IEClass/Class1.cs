a7c73f80521a40e4235000fd5cad5acec90da06b
MertGuney <mert_guney_29@hotmail.com>
2020-06-26 10:54:57 +0300

    mertguney.com ilk publish , mail işlemleri ayarlanacak

---
+++ MJG/Models/IEClass/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MJG.Models.Entity;
+
+namespace MJG.Models.IEClass
+{
+    public class Class1
+    {
+
+        public IEnumerable<tblHakkimda> Deger1 { get; set; }
+        public IEnumerable<tblHizmetler> Deger2 { get; set; }
+        public IEnumerable<tblDeneyim> Deger3 { get; set; }
+    }
+}
